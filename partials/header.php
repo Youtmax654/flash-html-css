c7a838b0503fd0018400c3b9ce107eacbab1ee74
Lomudru <kmejasson@gmail.com>
2023-10-31 10:12:41 +0100

    Change color of text of the current page for the header

--- partials/header.php
+++ partials/header.php
@@ -1,6 +1,6 @@
 <?php 
-    $current = $_SERVER['REQUEST_URI'];
-    if($current != "/flash_memory/index.php"):
+    $currentPage = $_SERVER['REQUEST_URI'];
+    if($currentPage != "/flash_memory/index.php"):
 ?>
 <header class="header">
         <p>The Power Of Memory</p>
@@ -8,13 +8,13 @@
         <input type="checkbox" id="ch">
         <nav class="header-right"> <!-- Navbar -->
             <ul>
-                <li><a href="<?=PROJECT_FOLDER?>index.php">ACCUEIL</a></li>
-                <li><a href="<?=PROJECT_FOLDER?>games/memory/index.php">JEU</a></li>
-                <li><a href="<?=PROJECT_FOLDER?>games/memory/scores.php">SCORES</a></li>
-                <li><a href="<?=PROJECT_FOLDER?>contact.php">NOUS CONTACTER</a></li>
-                <li><a href="<?=PROJECT_FOLDER?>login.php">CONNEXION</a></li>
-                <li><a href="<?=PROJECT_FOLDER?>register.php">INSCRIPTION</a></li>
-                <li><a href="<?=PROJECT_FOLDER?>myAccount.php">MON COMPTE</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>index.php" class="<?php echo PROJECT_FOLDER . "index.php" == $currentPage ? "current" : NULL; ?>">ACCUEIL</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>games/memory/index.php" class="<?php echo PROJECT_FOLDER . "games/memory/index.php" == $currentPage ? "current" : NULL; ?>">JEU</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>games/memory/scores.php" class="<?php echo  PROJECT_FOLDER . "games/memory/scores.php" == $currentPage ? "current" : NULL; ?>">SCORES</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>contact.php" class="<?php echo  PROJECT_FOLDER . "contact.php" == $currentPage ? "current" : NULL; ?>">NOUS CONTACTER</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>login.php" class="<?php echo  PROJECT_FOLDER . "login.php" == $currentPage ? "current" : NULL; ?>">CONNEXION</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>register.php" class="<?php echo  PROJECT_FOLDER . "register.php" == $currentPage ? "current" : NULL; ?>">INSCRIPTION</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>myAccount.php" class="<?php echo  PROJECT_FOLDER . "myAccount.php" == $currentPage ? "current" : NULL; ?>">MON COMPTE</a></li>
             </ul>
         </nav>
 </header>
@@ -28,7 +28,7 @@
         <input type="checkbox" id="ch">
         <nav class="header-right">
             <ul>
-                <li><a href="<?=PROJECT_FOLDER?>index.php">ACCUEIL</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>index.php" class="<?php echo  PROJECT_FOLDER . "index.php" == $currentPage ? "current" : NULL; ?>">ACCUEIL</a></li>
                 <li><a href="<?=PROJECT_FOLDER?>games/memory/index.php">JEU</a></li>
                 <li><a href="<?=PROJECT_FOLDER?>games/memory/scores.php">SCORES</a></li>
                 <li><a href="<?=PROJECT_FOLDER?>contact.php">NOUS CONTACTER</a></li>
