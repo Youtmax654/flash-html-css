47e465249ae862113c94a718c09a05b07027a186
Lomudru <kmejasson@gmail.com>
2023-10-31 09:33:04 +0100

    Adding changing header for the index

--- partials/header.php
+++ partials/header.php
@@ -1,3 +1,7 @@
+<?php 
+    $current = $_SERVER['REQUEST_URI'];
+    if($current != "/flash_memory/index.php"):
+?>
 <header class="header">
         <p>The Power Of Memory</p>
         <label for="ch" id="lab"></label>
@@ -14,21 +18,24 @@
             </ul>
         </nav>
 </header>
+<?php
+    else:
+?>
 
-
-<!-- <header class="header_index">
+<header class="header_index">
         <p>The Power Of Memory</p>
         <label for="ch" id="lab"></label>
         <input type="checkbox" id="ch">
         <nav class="header-right">
             <ul>
                 <li><a href="<?=PROJECT_FOLDER?>index.php">ACCUEIL</a></li>
-                <li><a href="<?=PROJECT_FOLDER?>games/index.php">JEU</a></li>
-                <li><a href="<?=PROJECT_FOLDER?>games/scores.php">SCORES</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>games/memory/index.php">JEU</a></li>
+                <li><a href="<?=PROJECT_FOLDER?>games/memory/scores.php">SCORES</a></li>
                 <li><a href="<?=PROJECT_FOLDER?>contact.php">NOUS CONTACTER</a></li>
                 <li><a href="<?=PROJECT_FOLDER?>login.php">CONNEXION</a></li>
                 <li><a href="<?=PROJECT_FOLDER?>register.php">INSCRIPTION</a></li>
                 <li><a href="<?=PROJECT_FOLDER?>myAccount.php">MON COMPTE</a></li>
             </ul>
         </nav>
-</header> -->
+</header>
+<?php endif ?>
