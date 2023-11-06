917049b35e9c8468c186459249a2b17a7cec3bed
Maxime Penn <59399621+Youtmax654@users.noreply.github.com>
2023-11-06 16:32:46 +0100

    bug fixed

--- assets/js/memory.js
+++ assets/js/memory.js
@@ -2,6 +2,6 @@ var cards = document.querySelectorAll(".card");
 
 [...cards].forEach((card)=>{
     card.addEventListener("click", function(){
-    card.classList.toggle("flipCard");
-});
+    card.classList.add("flipCard");
+    });
 });
